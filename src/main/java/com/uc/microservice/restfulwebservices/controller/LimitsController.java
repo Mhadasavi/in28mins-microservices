56d2c4165292a9a2461dfaa6e0fcd7d198acf37a
Upkar <Upkar@DESKTOP-22L0J3T>
2023-08-26 21:04:03 +0530

    Add initial files

---
+++ src/main/java/com/uc/microservice/restfulwebservices/controller/LimitsController.java
@@ -0,0 +1,20 @@
+package com.uc.microservice.restfulwebservices.controller;
+
+import com.uc.microservice.restfulwebservices.bean.Limits;
+import com.uc.microservice.restfulwebservices.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+//        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
---
+++ src/test/java/com/uc/microservice/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uc.microservice.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
